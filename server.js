c86c5a8b02965867cd31b01f906d0684e15d5df3
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-07 09:45:07 -0300

    com4

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-// server.mjs
+/*
+// server.js
 import { createServer } from 'node:http';
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -8,4 +9,33 @@ const server = createServer((req, res) => {
 server.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
-// run with `node server.mjs`
+*/
+
+// run with `node server.js`
+/*
+FrameWork
+POST localhost:3000/videos
+DELETE localhost:3000/videos/1
+Instalar o microframework fastify no terminal através do comando npm install fastify
+*/
+
+//Com o fastify
+
+import { fastify } from 'fastify'
+const server = fastify()
+
+server.get('/' , () => {
+  return 'Hi People!'
+})
+
+server.get('/hi' , () => {
+  return 'Hi Brasil!'
+})
+
+server.get('/node' , () => {
+  return 'Hi Node.js'
+})
+
+server.listen({
+  port: 3000,
+})
